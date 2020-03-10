411941f9d78d0aa382cb5d0efdada9c0a5a4ac3b
kkon999 <60930890+kkon999@users.noreply.github.com>
2020-03-10 23:25:08 +0900

    Add files via upload

---
+++ vsjava/java_test02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner; 
+
+public class java_test02 { 
+
+public static void main(String[] args) { 
+Scanner sc = new Scanner(System.in); 
+while(sc.hasNext()) 
+{ System.out.println(sc.nextLine());
+ } 
+}
+}
